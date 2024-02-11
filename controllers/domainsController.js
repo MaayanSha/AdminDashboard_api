28aefb3d05bc1243c8350393116156aea020fd34
maayan <maayansh.ms@gmail.com>
2024-02-11 15:16:43 +0200

    fixed minor issues

--- controllers/domainsController.js
+++ controllers/domainsController.js
@@ -3,11 +3,21 @@ const {verifyNameAndAdd, removeDomain} = require('./domainNames')
 
 const publishersDataBase = {
     publishers: require('../models/publishers.json'),
-    setPublishers: (publishers) => this.publishers = publishers
 }
 
 const getAllPublishers = (req, res) => {
     res.status(200).json(publishersDataBase.publishers)
+}
+
+//add a new publisher to the database
+const addPublisher = (req, res) => {
+    const {publisher} = req.body
+    const newPublisher = {
+        publisher: publisher,
+        domains: [],
+    }
+    publishersDataBase.publishers.push(newPublisher)
+    res.status(200).json(newPublisher)
 }
 
 //:GET
@@ -25,9 +35,9 @@ const getAllDomains = (req, res) => {
 //:POST
 //add a new domain under a specific publisher
 const addNewDomain = (req, res) => {
-    const {domainName, desktopAds, mobileAds, publisher} = req.body
+    const {domain, desktopAds, mobileAds, publisher} = req.body
     //check if domain name is available
-    const isAvailable = verifyNameAndAdd(domainName)
+    const isAvailable = verifyNameAndAdd(domain)
     //if domain name already taken, return error
     if (!isAvailable){
         return res.status(409).json({message: "Error: domainName is unavailable."})
@@ -35,7 +45,7 @@ const addNewDomain = (req, res) => {
     //create new domain object
     const newDomain = 
     {
-        "domain": domainName,
+        "domain": domain,
         "desktopAds": Number(desktopAds),
         "mobileAds": Number(mobileAds)
       }
@@ -44,22 +54,23 @@ const addNewDomain = (req, res) => {
     //append the new domain at the domain list of the publisher
     publishersDataBase.publishers[publisherObjIndex].domains.push(newDomain)
     //return the updated domain list
-    res.status(200).json({message: "new domain added succesfully"})
+    res.status(200).json(newDomain)
 }
 
 //:PUT
 //edit an existing domain under a specific publisher
 const editExistingDomain = (req, res) => {
-    const {domainName, desktopAds, mobileAds, publisher} = req.body
+    const {domain, desktopAds, mobileAds, publisher} = req.body
     //check if there is a new domain name in the request
     const newDomain = req.body?.newDomain
+    const oldDomain = domain
     //if the domain name should be updated, update it in the name list
     if (newDomain){
-        removeDomain(domainName)
+        removeDomain(domain)
         verifyNameAndAdd(newDomain)
     }
     const updatedDomain = {
-        "domain": newDomain || domainName,
+        "domain": newDomain || domain,
         "desktopAds": Number(desktopAds),
         "mobileAds": Number(mobileAds)
     }
@@ -67,22 +78,23 @@ const editExistingDomain = (req, res) => {
     //find the publisher location in the DB
     const publisherObjIndex = publishersDataBase.publishers.findIndex(obj => obj.publisher === publisher)
     //find the old domain location in the DB under the publisher
-    const domainIndex = publishersDataBase.publishers[publisherObjIndex].domains.findIndex(domain => domain.domain === domainName)
+    const domainIndex = publishersDataBase.publishers[publisherObjIndex].domains.findIndex(domain => domain.domain === oldDomain)
+    console.log(domainIndex)
     //set the updated domain properties
     publishersDataBase.publishers[publisherObjIndex].domains[domainIndex] = updatedDomain
 
-    res.status(200).json({message: "domain edited succesfully"})
+    res.status(200).json(updatedDomain)
 
 }
 
 //:DELETE
 //delete an existing domain under a specific publisher
 const deleteDomain = (req, res) => {
-    const {domainName, publisher} = req.body
+    const {domain, publisher} = req.headers
     //find the publisher location in the DB
     const publisherObjIndex = publishersDataBase.publishers.findIndex(obj => obj.publisher === publisher)
     //find the old domain location in the DB under the publisher
-    const domainIndex = publishersDataBase.publishers[publisherObjIndex].domains.findIndex(domain => domain.domain === domainName)
+    const domainIndex = publishersDataBase.publishers[publisherObjIndex].domains.findIndex(domain => domain.domain === domain)
     //if found the domain under the publisher, remove the domain from the list.
     if (domainIndex){
         publishersDataBase.publishers[publisherObjIndex].domains.pop(domainIndex)
@@ -93,6 +105,7 @@ const deleteDomain = (req, res) => {
 
 module.exports = {
     getAllPublishers,
+    addPublisher,
     getAllDomains,
     addNewDomain,
     editExistingDomain,
